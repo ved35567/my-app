02f8e3ac66c6bea9bfddc448a0e24b985ae7c9fb
ved35567 <vedvyassahu2001@gmail.com>
2025-01-31 22:25:21 +0530

    updates

--- src/Component/NavBar.js
+++ src/Component/NavBar.js
@@ -1,35 +1,51 @@
 import "../Design/NavContainer.css";
 import "../Design/ButtonModul.css";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import navtitle from "./Image/navtitle/Spardha.png";
 
 const NavBar = () => {
-  return (
-    <div className="Container">
-      <img className="HeaderName" src={navtitle} alt="Spardha img" />
-      <ul>
-        <li className="list-box">
-          <Link to="/">Home</Link>
-        </li>
-        <li className="list-box">
-          <Link to="/About">About</Link>
-        </li>
+  const [display, setDisplay] = useState(false);
+  function setIsOpen() {
+    let active = document.getElementsByClassName("hide-display")[0]; // Get the first element
+    if (active) {
+      active.classList.toggle("show-menu"); // Toggle the class
+    }
+    setDisplay(!display); // Update state
+  }
 
-        <li>
-          <Link to="/Event">Event</Link>
-        </li>
-        <li>
-          <Link to="/Results">Results</Link>
-        </li>
-        <li>
-          {" "}
-          <Link to="/Contact"> Contact</Link>
-        </li>
+  return (
+    <div>
+      <nav className="Container">
+        <img className="HeaderName" src={navtitle} alt="Spardha img" />
+        <ul className="hide-display">
+          <li className="list-box">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="list-box">
+            <Link to="/About">About</Link>
+          </li>
+
+          <li>
+            <Link to="/Event">Event</Link>
+          </li>
+          <li>
+            <Link to="/Results">Results</Link>
+          </li>
+          <li>
+            {" "}
+            <Link to="/Contact"> Contact</Link>
+          </li>
+        </ul>
+
         <div className="btn">
           <button className="Registration">Registration</button>
           <button className="Login">Login</button>
         </div>
-      </ul>
+        <div className="harm-burger" onClick={setIsOpen}>
+          ☰
+        </div>
+      </nav>
     </div>
   );
 };
